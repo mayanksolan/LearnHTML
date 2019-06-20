d9db64e08c9b294804f14dffb354b1dd01e9cf01
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-06-20 17:56:36 +0530

    changes

---
+++ React_Scrimba/P1/first-app/src/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Mayank Solan</h1>
+            <p>A budding Web Developer</p>
+            <ol>
+                <li>Mauritius</li>
+                <li>Sweden</li>
+                <li>New Zealand</li>
+            </ol>
+        </div>
+    )
+}
+
+export default MyInfo;
--- React_Scrimba/P1/first-app/src/index.js
+++ React_Scrimba/P1/first-app/src/index.js
@@ -4,21 +4,25 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function MyInfo() {
-    return (
-    <div>
-      <h1>Mayank Solan</h1>
-      <p>A budding Web Developer</p>
-      <ol>
-        <li>Mauritius</li>
-        <li>Sweden</li>
-        <li>New Zealand</li>
-      </ol>
-    </div>
-    )
-  }
+import MyInfo from "./MyInfo"
+
+// function MyInfo() {
+//     return (
+//         <div>
+//             <h1>Mayank Solan</h1>
+//             <p>A budding Web Developer</p>
+//             <ol>
+//                 <li>Mauritius</li>
+//                 <li>Sweden</li>
+//                 <li>New Zealand</li>
+//             </ol>
+//         </div>
+//     )
+// }
 
 ReactDOM.render(<MyInfo />, document.getElementById('root'));
+
+//ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
