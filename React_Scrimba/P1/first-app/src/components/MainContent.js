97d28bb9f4d140416c8079cb8566d2c488e92117
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-06-21 12:42:54 +0530

    more changes

--- React_Scrimba/P1/first-app/src/components/MainContent.js
+++ React_Scrimba/P1/first-app/src/components/MainContent.js
@@ -1,9 +1,27 @@
 import React from "react"
 
 function MainContent() {
+    const date = new Date(2018, 6, 31, 18)
+    const hours = date.getHours()
+    let timeOfDay
+    const styles = {
+        fontSize: 30
+    }
+
+    if (hours < 12) {
+        timeOfDay = "morning"
+        styles.color = "#04756F"
+    } else if (hours >= 12 && hours < 17) {
+        timeOfDay = "afternoon"
+        styles.color = "#8914A3"
+    } else {
+        timeOfDay = "night"
+        styles.color = "#D90000"
+    }
     return (
         <div>
-            <main>Hi from Mayank</main>
+            <h1>Hi from Mayank</h1>
+            <h3 style={styles}>Good {timeOfDay}!</h3>
             <p>Top Places to visit</p>
             <ul>
                 <li>New Zealand</li>
--- React_Scrimba/todo-list/src/App.js
+++ React_Scrimba/todo-list/src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
+import ToDoItem from "./components/ToDoItem"
 
 function App() {
     return (
-        <div>
-            <input type="checkbox" name="chk1" value="chk1" /><span> Checkbox 1</span><br/>
-            <input type="checkbox" name="chk2" value="chk2" /><span> Checkbox 2</span><br/>
-            <input type="checkbox" name="chk3" value="chk3" /><span> Checkbox 3</span><br/>
-        </div>
+        <div className="todo-list">
+            <ToDoItem />
+            <ToDoItem />
+            <ToDoItem />
+            <ToDoItem />
+        </div>        
     )
 }
 
