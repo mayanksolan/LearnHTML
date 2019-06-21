bb0625f0f5339fa6556c66db11cec178f27177b4
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-06-21 12:10:07 +0530

    todo

---
+++ React_Scrimba/todo-list/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox" name="chk1" value="chk1" /><span> Checkbox 1</span><br/>
+            <input type="checkbox" name="chk2" value="chk2" /><span> Checkbox 2</span><br/>
+            <input type="checkbox" name="chk3" value="chk3" /><span> Checkbox 3</span><br/>
+        </div>
+    )
+}
+
+export default App;
