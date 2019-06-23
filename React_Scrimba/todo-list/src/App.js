d6683fd63a5662034a8269744cffdd64d5c925a1
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-06-23 13:37:04 +0530

    function to class

--- React_Scrimba/todo-list/src/App.js
+++ React_Scrimba/todo-list/src/App.js
@@ -2,13 +2,19 @@ import React from "react"
 import ToDoItem from "./components/ToDoItem"
 import todosData from "./todosData"
 
-function App() {
-    const todoComponent = todosData.map(todo => <ToDoItem key={todo.id} todo={todo} />)
-    return (
-        <div className="todo-list">
-            {todoComponent}
-        </div>        
-    )
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {todosData:todosData}
+    }
+    render() {
+        const todoComponent = this.state.todosData.map(todo => <ToDoItem key={todo.id} todo={todo} />)
+        return (
+            <div className="todo-list">
+                {todoComponent}
+            </div>        
+        )
+    }    
 }
 
 export default App;
