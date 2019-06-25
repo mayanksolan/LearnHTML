85d80c9052503eafeb95419ba5cad2b5a77e90a6
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-06-25 15:55:20 +0530

    ApprovalCard

--- React_Stephen/components/src/index.js
+++ React_Stephen/components/src/index.js
@@ -1,15 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail"
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <CommentDetail />            
-            <CommentDetail />
-            <CommentDetail />
-        </div>
-    );
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Vibhu"
+          timeAgo="Today at 4:45 PM"
+          content="Good Work"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Gaurav"
+          timeAgo="Today at 6:00 PM"
+          content="Nice!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Harsh"
+          timeAgo="Today at 7:30 PM"
+          content="Bhadiya hain"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
