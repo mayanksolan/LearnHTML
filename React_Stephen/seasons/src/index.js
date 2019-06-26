c92b95f2e727f6cf4054e1e01bd9ae7344292b9a
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-06-26 16:04:45 +0530

    season 2

--- React_Stephen/seasons/src/index.js
+++ React_Stephen/seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   // constructor(props) {
@@ -17,14 +18,22 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>latitude: {this.state.lat}</div>
+      return <div><SeasonDisplay lat={this.state.lat} /></div>
     }
-    return <div>Loading!</div>
+    return <Spinner message="Please accept location request" />
+  }
+
+  render() {
+    return (
+      <div className="border-green">
+        {this.renderContent()}
+      </div>
+    )
   }
 }
 
