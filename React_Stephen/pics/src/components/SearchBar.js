526ef8bc7fc2f041b7034d4e2f0de9b0d1c8b67d
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-06-27 15:24:50 +0530

    pics3

--- React_Stephen/pics/src/components/SearchBar.js
+++ React_Stephen/pics/src/components/SearchBar.js
@@ -1,21 +1,30 @@
-import React from "react"
+import React from "react";
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
-    render() {
-        return (
-            <div className="ui segment">
-                <form className="ui form">
-                    <div className="field">
-                        <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange} />
-                    </div>
-                </form>
-            </div >
-        )
-    }
+  state = { term: "" };
+
+  onFormSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+            {/*<input type="text" onChange={(e) => console.log(e.target.value)} />*/}
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default SearchBar
+export default SearchBar;
