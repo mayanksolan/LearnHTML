6e61a2bc4536d232324249c119c17eee80d4de8f
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-06-28 13:33:11 +0530

    pics5

--- React_Stephen/pics/src/components/ImageList.js
+++ React_Stephen/pics/src/components/ImageList.js
@@ -1,3 +1,5 @@
+import "./ImageList.css";
+import ImageCard from "./ImageCard";
 import React from "react";
 
 const ImageList = props => {
@@ -6,10 +8,10 @@ const ImageList = props => {
   //       <img key={image.id} src={image.urls.regular} alt={image.description} />
   //     );
   //   });
-  const images = props.images.map(({ id, urls, description }) => {
-    return <img key={id} src={urls.regular} alt={description} />;
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
