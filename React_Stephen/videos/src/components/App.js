34cb9b563636ec5580a16b7b9783dc0dfe0094e3
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-07-01 12:55:52 +0530

    videos3

--- React_Stephen/videos/src/components/App.js
+++ React_Stephen/videos/src/components/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../api/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("Porsche");
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
       params: {
@@ -10,11 +18,32 @@ class App extends React.Component {
       }
     });
     console.log(response.data.items);
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
+  };
+  onVideoSelect = video => {
+    console.log("From the App!", video);
+    this.setState({ selectedVideo: video });
   };
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
