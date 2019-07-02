31cbbbf6fa09393d522ce26c0de53c4162517783
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-07-03 00:59:03 +0530

    songs5

--- React_Stephen/songs/src/components/SongList.js
+++ React_Stephen/songs/src/components/SongList.js
@@ -8,7 +8,12 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -16,12 +21,12 @@ class SongList extends Component {
     });
   }
   render() {
-    console.log(this.props);
     return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
 const mapStateToProps = state => {
+  //console.log(state);
   return { songs: state.songs };
 };
 
