1df2b487a118611f8819bffbc96b36368e0bb5bb
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-07-07 22:25:34 +0530

    vidideas

---
+++ Projects/VidIdeas/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+//connect to mongoose
+mongoose
+  .connect(
+    "mongodb+srv://devMays:devColdplay@cluster0-n0rgm.mongodb.net/vidjot?retryWrites=true",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB connected..."))
+  .catch(err => console.log(err));
+
+//Load Idea Model
+require("./models/Idea");
+const Idea = mongoose.model("ideas");
+
+//Handlebars Middlewares
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Index Route
+app.get("/", (req, res) => {
+  const title = "Hello There";
+  res.render("index", {
+    title: title
+  });
+});
+
+//About Route
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});
