8499c2a390e2e4b698840db45f3bed36555f0217
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-07-08 14:35:19 +0530

    vidideas7

--- Projects/VidIdeas/routes/ideas.js
+++ Projects/VidIdeas/routes/ideas.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const { ensureAuthenticated } = require("../helpers/auth");
 
 //Load Idea Model
 require("../models/Idea");
 const Idea = mongoose.model("ideas");
 
 //Idea index page
-router.get("/", (req, res) => {
-  Idea.find({})
+router.get("/", ensureAuthenticated, (req, res) => {
+  Idea.find({ user: req.user.id })
     .sort({ date: "desc" })
     .then(ideas => {
       res.render("ideas/index", {
@@ -18,23 +19,28 @@ router.get("/", (req, res) => {
 });
 
 //Add idea form
-router.get("/add", (req, res) => {
+router.get("/add", ensureAuthenticated, (req, res) => {
   res.render("ideas/add");
 });
 
 //Edit idea form
-router.get("/edit/:id", (req, res) => {
+router.get("/edit/:id", ensureAuthenticated, (req, res) => {
   Idea.findOne({
     _id: req.params.id
   }).then(idea => {
-    res.render("ideas/edit", {
-      idea: idea
-    });
+    if (idea.user != req.user.id) {
+      req.flash("error_msg", "Not Authorized");
+      redirect("/ideas");
+    } else {
+      res.render("ideas/edit", {
+        idea: idea
+      });
+    }
   });
 });
 
 //Add process form
-router.post("/", (req, res) => {
+router.post("/", ensureAuthenticated, (req, res) => {
   let errors = [];
   if (!req.body.title) {
     errors.push({ text: "Please add a title" });
@@ -51,7 +57,8 @@ router.post("/", (req, res) => {
   } else {
     const newUser = {
       title: req.body.title,
-      details: req.body.details
+      details: req.body.details,
+      user: req.user.id
     };
     new Idea(newUser).save().then(idea => {
       req.flash("success_msg", "Video idea added");
@@ -76,7 +83,7 @@ router.put("/:id", (req, res) => {
 });
 
 //delete idea
-router.delete("/:id", (req, res) => {
+router.delete("/:id", ensureAuthenticated, (req, res) => {
   Idea.deleteOne({ _id: req.params.id }).then(() => {
     req.flash("success_msg", "Video idea removed");
     res.redirect("/ideas");
--- Projects/VidIdeas/routes/users.js
+++ Projects/VidIdeas/routes/users.js
@@ -79,4 +79,11 @@ router.post("/register", (req, res) => {
   }
 });
 
+//logout route
+router.get("/logout", (req, res) => {
+  req.logOut();
+  req.flash("success_msg", "You are logged out");
+  res.redirect("/users/login");
+});
+
 module.exports = router;
