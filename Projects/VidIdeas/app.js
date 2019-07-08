aa07b58854c2761e8d9678a9003edb8eb6dcd876
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-07-08 13:01:23 +0530

    vidideas6

--- Projects/VidIdeas/app.js
+++ Projects/VidIdeas/app.js
@@ -5,6 +5,7 @@ const methodOverride = require("method-override");
 const flash = require("connect-flash");
 const session = require("express-session");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -12,6 +13,9 @@ const app = express();
 //load routes
 const ideas = require("./routes/ideas");
 const users = require("./routes/users");
+
+//passport config
+require("./config/passport")(passport);
 
 //Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
@@ -48,6 +52,10 @@ app.use(
   })
 );
 
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
 //global variables
@@ -55,6 +63,7 @@ app.use((req, res, next) => {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
   res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
   next();
 });
 
