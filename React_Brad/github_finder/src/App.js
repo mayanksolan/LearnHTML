e9f9678d290b613155460897ff71bd410f971e26
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-07-09 13:38:08 +0530

    github_finder3

--- React_Brad/github_finder/src/App.js
+++ React_Brad/github_finder/src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 import "./App.css";
+import axios from "axios";
 
 class App extends React.Component {
+  componentDidMount() {
+    axios.get("https://api.github.com/users");
+  }
   render() {
-    const name = "Mayank Solan";
     return (
       <div className="App">
-        <h1>Hello {name}</h1>
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
