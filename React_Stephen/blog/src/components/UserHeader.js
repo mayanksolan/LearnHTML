493580c71bdade65115207d1d942b54a1247b8c5
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-07-22 03:06:30 +0530

    blog wrap up loadash

--- React_Stephen/blog/src/components/UserHeader.js
+++ React_Stephen/blog/src/components/UserHeader.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchUser } from "../actions";
+//import { fetchUser } from "../actions";
 
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
+  //   componentDidMount() {
+  //     this.props.fetchUser(this.props.userId);
+  //   }
   render() {
     const { user } = this.props;
     if (!user) {
@@ -20,6 +20,6 @@ const mapStateToProps = (state, ownProps) => {
 };
 
 export default connect(
-  mapStateToProps,
-  { fetchUser }
+  mapStateToProps
+  //,  { fetchUser }
 )(UserHeader);
