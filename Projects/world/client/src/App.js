0d2876f63ea8721997dc917f30dce4d3ae439b39
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-07-24 08:46:01 +0530

    archive_world

--- Projects/world/client/src/App.js
+++ Projects/world/client/src/App.js
@@ -2,14 +2,23 @@ import React from "react";
 // eslint-disable-next-line
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import countriesApi from "../src/api/countriesApi";
 
 class App extends React.Component {
+  state = { countriesData: [] };
+  getData = async term => {
+    const response = await countriesApi.get();
+    this.setState({ countriesData: response.data });
+  };
   render() {
     return (
       <Router>
         <div>
           <Navbar />
           App
+          <form getData={this.getData}>
+            <button>Click Me</button>
+          </form>
         </div>
       </Router>
     );
