4ea514cc1a7a6ac69d3a2b8733d57cfc456baedd
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-08-05 18:02:33 +0530

    path and fs demo node

--- React_Brad/Node_Crash_Course/reference/path_demo.js
+++ React_Brad/Node_Crash_Course/reference/path_demo.js
@@ -10,4 +10,7 @@ console.log(path.dirname(__filename));
 console.log(path.extname(__filename));
 
 //create path object
-console.log(path.parse(__filename));
+console.log(path.parse(__filename).base);
+
+//Concatenate paths
+console.log(path.join(__dirname, "test", "hello.html"));
