26fac7f160e6de001c35bf6d17ec1799123945a4
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-08-05 13:10:32 +0530

    poketimes final

--- Projects/poketimes/src/App.js
+++ Projects/poketimes/src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Post from "./components/Post";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/About" component={About} />
-        <Route exact path="/Contact" component={Contact} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/Contact" component={Contact} />
+          <Route path="/:post_id" component={Post} />
+        </Switch>
       </div>
     </Router>
   );
--- Projects/poketimes/src/components/Home.js
+++ Projects/poketimes/src/components/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import Pokeball from "../pokeball.png";
 
 class Home extends React.Component {
   state = {
@@ -19,8 +21,11 @@ class Home extends React.Component {
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="A Pokeball" />
             <div className="card-content">
-              <span className="card-title">{post.title}</span>
+              <Link to={"/" + post.id}>
+                <span className="card-title red-text">{post.title}</span>
+              </Link>
               <p>{post.body}</p>
             </div>
           </div>
@@ -30,7 +35,7 @@ class Home extends React.Component {
       <div className="center">No Posts Yet</div>
     );
     return (
-      <div className="container">
+      <div className="container home">
         <h4 className="center">Home</h4>
         {postList}
       </div>
