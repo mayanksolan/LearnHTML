ba4a31c46fc5a23b979a36f04bb5c02edb130f62
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-08-09 01:37:54 +0530

    action creators

--- Projects/poketimes/src/components/Post.js
+++ Projects/poketimes/src/components/Post.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { deletePost } from "../actions/postActions";
 
 export class Post extends Component {
   handleClick = () => {
@@ -35,7 +36,7 @@ const mapStateToProps = (state, ownProps) => {
 const mapDispatchToProps = dispatch => {
   return {
     deletePost: id => {
-      dispatch({ type: "DELETE_POST", id: id });
+      dispatch(deletePost(id));
     }
   };
 };
