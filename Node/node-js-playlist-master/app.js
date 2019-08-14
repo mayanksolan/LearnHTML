33a67226571f25721c0a1132d1bd13de14fed5d9
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-08-14 11:59:22 +0530

    request server

--- Node/node-js-playlist-master/app.js
+++ Node/node-js-playlist-master/app.js
@@ -1,6 +1,10 @@
 let http = require("http");
 
 let server = http.createServer(function(req, res) {
+  console.log("request was made from " + req.url);
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hey People");
 });
+
+server.listen(3000, "127.0.0.1");
+console.log("You are now listening to port 3000");
