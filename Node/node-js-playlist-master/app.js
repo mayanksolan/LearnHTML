3abd35f97646bcd944c0e18651eac537fb53b397
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-08-16 11:35:05 +0530

    module patterns node

--- Node/node-js-playlist-master/app.js
+++ Node/node-js-playlist-master/app.js
@@ -1,10 +1,17 @@
-let http = require("http");
+//create server
+// let http = require("http");
 
-let server = http.createServer(function(req, res) {
-  console.log("request was made from " + req.url);
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hey People");
-});
+// let server = http.createServer(function(req, res) {
+//   console.log("request was made from " + req.url);
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end("Hey People");
+// });
 
-server.listen(3000, "127.0.0.1");
-console.log("You are now listening to port 3000");
+// server.listen(3000, "127.0.0.1");
+// console.log("You are now listening to port 3000");
+
+//Module/Require
+let countMethods = require("./count");
+console.log(countMethods.counter(["foo", "baz", "hoo"]));
+console.log(countMethods.firstElement(["foo", "baz", "hoo"]));
+console.log(countMethods.pi);
---
+++ Node/node-js-playlist-master/count.js
@@ -0,0 +1,9 @@
+let counter = arr => {
+  return `There are ${arr.length} elements in this array`;
+};
+let firstElement = arr => {
+  return `The first element of the arr is ${arr[0]}`;
+};
+let pi = 3.142;
+
+module.exports = { counter, firstElement, pi };
