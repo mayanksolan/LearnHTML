279c68e37c37dacc0e5975674553dd07e5f8817d
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-08-16 15:36:54 +0530

    mkdir node async

--- Node/node-js-playlist-master/app.js
+++ Node/node-js-playlist-master/app.js
@@ -1,1 +1,12 @@
 let fs = require("fs");
+
+fs.mkdir("stuff", function() {
+  console.log("directory created");
+  fs.readFile("readme.txt", "utf8", function(err, data) {
+    console.log(data);
+    fs.writeFile("./stuff/writeme.txt", data, err => {
+      if (err) throw err;
+      console.log("New file created adn data written");
+    });
+  });
+});
