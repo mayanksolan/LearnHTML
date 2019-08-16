3c6db253b91ce35746ddb0e92c556c55e2824009
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-08-16 16:14:08 +0530

    changes dir node

--- Node/node-js-playlist-master/app.js
+++ Node/node-js-playlist-master/app.js
@@ -1,12 +1,18 @@
 let fs = require("fs");
 
-fs.mkdir("stuff", function() {
-  console.log("directory created");
-  fs.readFile("readme.txt", "utf8", function(err, data) {
-    console.log(data);
-    fs.writeFile("./stuff/writeme.txt", data, err => {
-      if (err) throw err;
-      console.log("New file created adn data written");
-    });
+// fs.mkdir("stuff", function() {
+//   console.log("directory created");
+//   fs.readFile("readme.txt", "utf8", function(err, data) {
+//     console.log(data);
+//     fs.writeFile("./stuff/writeme.txt", data, err => {
+//       if (err) throw err;
+//       console.log("New file created adn data written");
+//     });
+//   });
+// });
+
+fs.unlink("./stuff/writeMe.txt", () => {
+  fs.rmdir("stuff", () => {
+    console.log("Directory removed");
   });
 });
