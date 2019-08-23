3665351a59edf5f17a61b8eb9c69327fa5192cb1
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-08-24 03:14:52 +0530

    movieapp images

--- Projects/movieapp/src/components/MovieItem.js
+++ Projects/movieapp/src/components/MovieItem.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 export default function MovieItem(props) {
-  console.log(props.movie);
-  return <div>{props.movie.Title}</div>;
+  //console.log(props.movie);
+  return (
+    <div>
+      <h3>{props.movie.Title}</h3>
+      <h6>{props.movie.Year}</h6>
+      <img src={props.movie.Poster} alt="poster" />
+    </div>
+  );
 }
