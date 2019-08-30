e7cfcb8a05da18890e53971a9b0802050f7a44b6
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-08-31 04:31:35 +0530

    changes

--- Node/ApiCreation/app.js
+++ Node/ApiCreation/app.js
@@ -3,13 +3,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!!!");
 });
 
 app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000....");
+app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params);
+});
+
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}....`);
 });
