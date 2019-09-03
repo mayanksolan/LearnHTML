c8c7821978a1752f727bdde1b5460029b9aa3fb8
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-09-03 16:04:30 +0530

    year month express app get

--- Node/ApiCreation/app.js
+++ Node/ApiCreation/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!!!, Keep Rocking");
+  res.send("Hello World!!!, You Keep Rocking");
 });
 
 app.get("/api/courses", (req, res) => {
@@ -11,6 +11,10 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.get("/api/courses/:id", (req, res) => {
+  res.send(req.params);
+});
+
+app.get("/api/courses/:year/:month", (req, res) => {
   res.send(req.params);
 });
 
