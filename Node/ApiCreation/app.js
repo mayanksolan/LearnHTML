ebbaa8f0bc98c3a481be6587f73490554cc7809c
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-09-04 08:19:01 +0530

    req params

--- Node/ApiCreation/app.js
+++ Node/ApiCreation/app.js
@@ -2,6 +2,12 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
+const courses = [
+  { id: 1, name: "Maths" },
+  { id: 2, name: "Physics" },
+  { id: 3, name: "Chemistry" }
+];
+
 app.get("/", (req, res) => {
   res.send("Hello World!!!, You Keep Rocking");
 });
@@ -11,12 +17,21 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.get("/api/courses/:id", (req, res) => {
-  res.send(req.params);
+  let course = courses.find(course => course.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("This course with the given id not found");
+  } else {
+    res.send(course);
+  }
 });
 
-app.get("/api/courses/:year/:month", (req, res) => {
-  res.send(req.params);
-});
+// app.get("/api/courses/:year/:month", (req, res) => {
+//   res.send(req.params);
+// });
+
+// app.get("/api/courses/:year/:month", (req, res) => {
+//   res.send(req.query);
+// });
 
 //PORT
 const port = process.env.PORT || 3000;
