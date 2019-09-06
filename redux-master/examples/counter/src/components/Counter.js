e68d953a245e4ae25a414aa672bbdf97ca41604e
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-09-06 11:00:33 +0530

    remove bind

--- redux-master/examples/counter/src/components/Counter.js
+++ redux-master/examples/counter/src/components/Counter.js
@@ -2,19 +2,13 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.incrementAsync = this.incrementAsync.bind(this);
-    this.incrementIfOdd = this.incrementIfOdd.bind(this);
-  }
-
-  incrementIfOdd() {
+  incrementIfOdd = () => {
     if (this.props.value % 2 !== 0) {
       this.props.onIncrement()
     }
   }
 
-  incrementAsync() {
+  incrementAsync = () => {
     setTimeout(this.props.onIncrement, 1000)
   }
 
@@ -22,23 +16,10 @@ class Counter extends Component {
     const { value, onIncrement, onDecrement } = this.props
     return (
       <p>
-        Clicked: {value} times
-        {' '}
-        <button onClick={onIncrement}>
-          +
-        </button>
-        {' '}
-        <button onClick={onDecrement}>
-          -
-        </button>
-        {' '}
-        <button onClick={this.incrementIfOdd}>
-          Increment if odd
-        </button>
-        {' '}
-        <button onClick={this.incrementAsync}>
-          Increment async
-        </button>
+        Clicked: {value} times <button onClick={onIncrement}>+</button>{' '}
+        <button onClick={onDecrement}>-</button>{' '}
+        <button onClick={this.incrementIfOdd}>Increment if odd</button>{' '}
+        <button onClick={this.incrementAsync}>Increment async</button>
       </p>
     )
   }
