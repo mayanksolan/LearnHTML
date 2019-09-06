d6e4315681f9a58b567a013afd58a5ab0fa9f122
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-09-06 15:34:35 +0530

    counter-mays

--- redux-master/examples/counter-mays/src/App.js
+++ redux-master/examples/counter-mays/src/App.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { createStore } from 'redux'
+import Counter from '../src/components/Counter'
+import reducer from '../src/reducers/index'
 
-function App() {
-  return <div className="App">Hello There!!!</div>
+const store = createStore(reducer)
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Counter
+          value={store.getState()}
+          onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+          onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+        />
+      </div>
+    )
+  }
 }
 
 export default App
---
+++ redux-master/examples/counter-mays/src/components/Counter.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Counter extends React.Component {
+  incrementIfOdd = () => {
+    if (this.props.value % 2 !== 0) {
+      this.props.onIncrement()
+    }
+  }
+  incrementIfEven = () => {
+    if (this.props.value % 2 === 0) {
+      this.props.onIncrement()
+    }
+  }
+  render() {
+    const { value, onIncrement, onDecrement } = this.props
+    console.log(this.props)
+    return (
+      <div>
+        Current Value is {value}
+        <div>
+          <button onClick={onIncrement}>+</button>
+          <button onClick={onDecrement}>-</button>
+        </div>
+        <div>
+          <button onClick={this.incrementIfOdd}>Increment if odd</button>
+          <button onClick={this.incrementIfEven}>Increment if even</button>
+        </div>
+        <div>
+          <button>Increment async</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired
+}
+
+export default Counter
