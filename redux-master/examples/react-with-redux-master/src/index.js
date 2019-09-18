f456e1094df64ab426392be3975ab451d6bd25fa
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-09-18 12:21:38 +0530

    applymiddleware redux

---
+++ redux-master/examples/react-with-redux-master/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reducer from './store/reducer'
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
+const logAction = store => {
+  return next => {
+    return action => {
+      const result = next(action)
+      console.log(`Caught in the middleware ${JSON.stringify(result)}`)
+      return result
+    }
+  }
+}
+
+const store = createStore(reducer, applyMiddleware(logAction))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
