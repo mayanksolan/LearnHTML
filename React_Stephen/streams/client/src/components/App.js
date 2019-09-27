6e15be6a52446ccc58005245c898910512c2cf8b
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-09-27 18:48:01 +0530

    Stream Routes

--- React_Stephen/streams/client/src/components/App.js
+++ React_Stephen/streams/client/src/components/App.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamList from "./streams/StreamList";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
-  return <div>
-    <BrowserRouter>
+  return (
     <div>
-      <Route exact path="/" component={} />
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={StreamList} />
+          <Route exact path="/streams/new" component={StreamCreate} />
+          <Route exact path="/streams/edit" component={StreamEdit} />
+          <Route exact path="/streams/delete" component={StreamDelete} />
+          <Route exact path="/streams/show" component={StreamShow} />
+        </div>
+      </BrowserRouter>
     </div>
-    </BrowserRouter>
-  </div>
+  );
 };
 
 export default App;
