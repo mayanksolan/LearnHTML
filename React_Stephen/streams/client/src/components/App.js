fbe5e017a1a9501cecc07ef8fadad0733c286ac1
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-09-28 21:02:04 +0530

    Streams Header setup

--- React_Stephen/streams/client/src/components/App.js
+++ React_Stephen/streams/client/src/components/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamList from "./streams/StreamList";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route exact path="/" component={StreamList} />
           <Route exact path="/streams/new" component={StreamCreate} />
           <Route exact path="/streams/edit" component={StreamEdit} />
