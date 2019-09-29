d01b4c85516ec4c5a87ca96fd8dbee61d0eea350
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-09-30 00:36:15 +0530

    google oauth streamy

--- React_Stephen/streams/client/src/components/GoogleAuth.js
+++ React_Stephen/streams/client/src/components/GoogleAuth.js
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "728173651275-j1el5d7bod3fj0bkc2v1nj1g3itic7nv.apps.googleusercontent.com",
-        scope: "email"
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "728173651275-j1el5d7bod3fj0bkc2v1nj1g3itic7nv.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          console.log(this);
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
   }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know if I am signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in!</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
