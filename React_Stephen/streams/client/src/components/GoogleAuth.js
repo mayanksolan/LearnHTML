d06d57812b09f5b3a0e22418739fccbc29433c76
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-09-29 19:24:35 +0530

    google oauth streams

---
+++ React_Stephen/streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "728173651275-j1el5d7bod3fj0bkc2v1nj1g3itic7nv.apps.googleusercontent.com",
+        scope: "email"
+      });
+    });
+  }
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
