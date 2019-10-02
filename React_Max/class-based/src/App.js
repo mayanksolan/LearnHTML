524c5be5a15ea42d2996832ad0dc03241691430a
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-10-02 14:21:54 +0530

    event target

--- React_Max/class-based/src/App.js
+++ React_Max/class-based/src/App.js
@@ -22,6 +22,16 @@ class App extends React.Component {
     });
   };
 
+  nameChangedHandler = e => {
+    this.setState({
+      persons: [
+        { name: "Max", age: 28 },
+        { name: e.target.value, age: 29 },
+        { name: "Stephanie", age: 27 }
+      ]
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -38,6 +48,7 @@ class App extends React.Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          changed={this.nameChangedHandler}
         >
           My Hobbies: Racing
         </Person>
