1b3741a4290d7d20f7b0b704e63c67f0859e6325
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-10-07 18:29:29 +0530

    password bcrypt

--- React_Brad/devconnector2/routes/api/users.js
+++ React_Brad/devconnector2/routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+const User = require("../../models/User");
 
 // @route   GET api/users..
 // @desc    Test route
@@ -15,11 +19,47 @@ router.post(
   [
     check("name", "Name is required")
       .not()
-      .isEmpty()
+      .isEmpty(),
+    check("email", "Include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
   ],
-  (req, res) => {
-    console.log(req.body);
-    res.send("User route");
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+
+    try {
+      //See if the user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+      //Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "PG",
+        d: "mm"
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+      //Encrypt password
+      const salt
+      //Return jasonwebtoken
+      res.send("User route");
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
