2c75408a4f54b5edf212c69ebbd146f18782af90
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-10-08 02:57:39 +0530

    add auth middleware

--- React_Brad/devconnector2/routes/api/users.js
+++ React_Brad/devconnector2/routes/api/users.js
@@ -70,7 +70,7 @@ router.post(
       jwt.sign(
         payload,
         config.get("jwtsecret"),
-        { expiresIn: 3600 },
+        { expiresIn: 360000 },
         (err, token) => {
           if (err) {
             throw err;
