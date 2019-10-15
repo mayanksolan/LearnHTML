8938d71e14242328e8805b611e2b7a342765bb60
Mayank Solan <mayanksolan@users.noreply.github.com>
2019-10-16 02:23:21 +0530

    set auth

--- React_Brad/devconnector2/client/src/actions/auth.js
+++ React_Brad/devconnector2/client/src/actions/auth.js
@@ -1,8 +1,33 @@
 import axios from "axios";
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import { setAlert } from "./alert";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
+} from "./types";
+import setAuthToken from "../utils/setAuthToken";
+
+//Load User
+export const loadUser = () => async dispatch => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get("/api/auth");
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data
+    });
+  } catch (error) {
+    dispatch({
+      type: AUTH_ERROR
+    });
+  }
+};
 
 //Register User
-export const Register = ({ name, email, password }) => async dispatch => {
+export const register = ({ name, email, password }) => async dispatch => {
   const config = {
     headers: {
       "Content-Type": "application/json"
@@ -16,6 +41,11 @@ export const Register = ({ name, email, password }) => async dispatch => {
       payload: res.data
     });
   } catch (error) {
+    console.log(error);
+    const errors = error.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
     dispatch({
       type: REGISTER_FAIL
     });
--- React_Brad/devconnector2/client/src/components/auth/Register.js
+++ React_Brad/devconnector2/client/src/components/auth/Register.js
@@ -4,8 +4,9 @@ import PropTypes from "prop-types";
 //import axios from "axios";
 import { Link } from "react-router-dom";
 import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -23,7 +24,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert("Passwords do not match", "danger", 3000);
     } else {
-      console.log("SUCCESS");
+      register({ name, email, password });
       // const newUser = {
       //   name,
       //   email,
@@ -59,7 +60,7 @@ const Register = ({ setAlert }) => {
             name="name"
             value={name}
             onChange={e => onChange(e)}
-            required
+            //required
           />
         </div>
         <div className="form-group">
@@ -69,7 +70,7 @@ const Register = ({ setAlert }) => {
             name="email"
             value={email}
             onChange={e => onChange(e)}
-            required
+            //required
           />
           <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
@@ -84,7 +85,7 @@ const Register = ({ setAlert }) => {
             minLength="6"
             value={password}
             onChange={e => onChange(e)}
-            required
+            //required
           />
         </div>
         <div className="form-group">
@@ -95,7 +96,7 @@ const Register = ({ setAlert }) => {
             minLength="6"
             value={password2}
             onChange={e => onChange(e)}
-            required
+            //required
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
@@ -108,10 +109,11 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 };
 
 export default connect(
   null,
-  { setAlert }
+  { setAlert, register }
 )(Register);
