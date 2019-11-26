44e59da354d877568b9972369e597748caa69864
Mayank Solan <mayanksolan@gmail.com>
2019-11-26 11:46:17 +0530

    contextApi

--- React_More/hello-world/src/App.js
+++ React_More/hello-world/src/App.js
@@ -1,25 +1,34 @@
 import React from "react";
 import "./App.css";
-import ClickCounterTwo from "./components/ClickCounterTwo";
-import HoverCounterTwo from "./components/HoverCounterTwo";
-import User from "./components/User";
-import Counter from "./components/Counter";
+// import ClickCounterTwo from "./components/ClickCounterTwo";
+// import HoverCounterTwo from "./components/HoverCounterTwo";
+// import User from "./components/User";
+// import Counter from "./components/Counter";
+import ComponentC from "./context/ComponentC";
+import { UserProvider } from "./context/userContext";
 
 function App() {
   return (
     <div className="App">
-      <Counter
-        render={(count, incrementCount) => (
-          <ClickCounterTwo count={count} incrementCount={incrementCount} />
-        )}
-      />
-      <Counter
-        render={(count, incrementCount) => (
-          <HoverCounterTwo count={count} incrementCount={incrementCount} />
-        )}
-      />
+      <UserProvider value="Mayank">
+        <ComponentC />
+      </UserProvider>
     </div>
   );
 }
 
 export default App;
+
+//render props
+
+//   <Counter
+//     render={(count, incrementCount) => (
+//       <ClickCounterTwo count={count} incrementCount={incrementCount} />
+//     )}
+//   />
+//   <Counter
+//     render={(count, incrementCount) => (
+//       <HoverCounterTwo count={count} incrementCount={incrementCount} />
+//     )}
+//   />
+// </div>
