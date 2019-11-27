4794d5019cd21f40e8f29b81c40c4727302c2db2
Mayank Solan <mayanksolan@gmail.com>
2019-11-27 11:47:53 +0530

    hookcounterthree

---
+++ React_More/learn-hooks/src/components/HookCounterThree.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState } from "react";
+
+function HookCounterThree() {
+  const [name, setName] = useState({ firstName: "", lastName: "" });
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          value={name.firstName}
+          onChange={e => setName({ firstName: e.target.value })}
+        />
+        <input
+          type="text"
+          value={name.lastName}
+          onChange={e => setName({ lastName: e.target.value })}
+        />
+        <h2>Your firstName is - {name.firstName}</h2>
+        <h2>Your lastName is - {name.lastName}</h2>
+      </form>
+    </div>
+  );
+}
+
+export default HookCounterThree;
