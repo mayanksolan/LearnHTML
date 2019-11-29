50cc5af37d07dd18f9ad9d35504947e550e37a1b
Mayank Solan <mayanksolan@gmail.com>
2019-11-29 17:07:37 +0530

    handleChange

--- Projects/moviebuff/src/components/layout/SearchBar.js
+++ Projects/moviebuff/src/components/layout/SearchBar.js
@@ -1,18 +1,34 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: ""
+    };
+  }
+  handleChange = e => {
+    this.setState(
+      {
+        searchText: e.target.value
+      },
+      () => console.log(this.state)
+    );
+  };
+
   render() {
     return (
       <div className="search">
         <div className="title">Search for a movie or show</div>
         <div>
-          <form>
-            <div>
-              <input type="text" placeholder="Type any Movie or show" />
-            </div>
-            <div>
-              <button type="submit">Submit</button>
-            </div>
+          <form onSubmit={this.formSubmit}>
+            <input
+              type="text"
+              name="searchMovie"
+              id="searchText"
+              placeholder="Type any Movie or show"
+              onChange={this.handleChange}
+            />
           </form>
         </div>
       </div>
