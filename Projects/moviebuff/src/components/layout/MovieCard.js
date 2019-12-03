2abb4b8c967b419149b2fbe1b0a8394d98094b12
Mayank Solan <mayanksolan@gmail.com>
2019-12-03 17:29:12 +0530

    button

--- Projects/moviebuff/src/components/layout/MovieCard.js
+++ Projects/moviebuff/src/components/layout/MovieCard.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 
 class MovieCard extends Component {
+  onClickHandler = e => {
+    console.log(e.target.value);
+  };
   render() {
     console.log(this.props.movie);
     // const [
@@ -14,17 +17,36 @@ class MovieCard extends Component {
     //   //   overview,
     //   //   release_date
     // ] = this.props.movie;
+
     const movieUrl =
       "https://image.tmdb.org/t/p/original/" + this.props.movie.poster_path;
     return (
       <div className="card">
-        <div className="container">
+        <div className="card-front">
           <img src={movieUrl} alt="Avatar" className="imageProp" />
         </div>
-        <h4>
-          <b>{this.props.movie.title}</b>
-        </h4>
-        <p>{this.props.movie.vote_average}</p>
+        <div className="card-back">
+          <div className="card-back-inside">
+            <h4>
+              Movie Name: <b>{this.props.movie.title}</b>
+            </h4>
+            <div className="card-details">
+              <div>
+                <h4>Rating: {this.props.movie.vote_average}</h4>
+                <h4>Released: {this.props.movie.release_date}</h4>
+              </div>
+              <p className="card-overview">
+                Overview: {this.props.movie.overview.substring(0, 50)}
+              </p>
+              <button
+                onClick={this.onClickHandler}
+                style={{ textDecoration: "underline" }}
+              >
+                Read More...
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
