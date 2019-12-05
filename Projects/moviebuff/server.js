7d44920b2f4b67e4ff24b08679730f9e0ecd34b6
Mayank Solan <mayanksolan@gmail.com>
2019-12-05 17:26:14 +0530

    dbconnect

---
+++ Projects/moviebuff/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+
+//connect database
+connectDB();
+
+//Init middleware
+// app.use(express.json({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send(req.body.name);
+});
+
+// define routes
+// app.use("/api/users", require("./routes/api/users"));
+// app.use("/api/auth", require("./routes/api/auth"));
+// app.use("/api/profile", require("./routes/api/profile"));
+// app.use("/api/posts", require("./routes/api/posts"));
+
+const PORT = process.env.port || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
