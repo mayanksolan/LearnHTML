0664a82dd7a3674a57e0138e1352c04b5bcd3f07
Mayank Solan <mayanksolan@gmail.com>
2019-12-06 13:34:04 +0530

    register action

--- Projects/moviebuff/client/src/actions/index.js
+++ Projects/moviebuff/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import setAuthToken from "../../src/utils/setAuthToken";
 
 export const setSearchText = searchText => {
   return {
@@ -23,4 +24,45 @@ export const setSearchMovieResults = searchMovieData => {
     type: "SET_SEARCH_MOVIE_RESULTS",
     payload: searchMovieData
   };
+};
+
+//load user
+export const loadUser = () => async dispatch => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get("/api/auth");
+    dispatch({
+      type: "USER_LOADED",
+      payload: res.data
+    });
+  } catch (error) {
+    dispatch({
+      type: "AUTH_ERROR"
+    });
+  }
+};
+
+//Register User
+export const register = ({ name, email, password }) => async dispatch => {
+  const config = { header: { "Content-Type": "application/json" } };
+  const body = JSON.stringify({ name, email, password });
+  try {
+    const res = await axios.post("/api/users", body, config);
+    dispatch({
+      type: "REGISTER_SUCCESS",
+      payload: res.data
+    });
+    dispatch(loadUser());
+  } catch (error) {
+    console.log(error);
+    const errors = error.response.data.errors;
+    // if (errors) {
+    //   errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    // }
+    dispatch({
+      type: "REGISTER_FAIL"
+    });
+  }
 };
--- Projects/moviebuff/routes/api/users.js
+++ Projects/moviebuff/routes/api/users.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+const config = require("config");
 const User = require("../../models/User");
 
 // @route   GET api/users..
