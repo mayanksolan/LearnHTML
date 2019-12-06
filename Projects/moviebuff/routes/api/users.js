600f0f20607de0c3300a62ebdca1e0cae58eac4c
Mayank Solan <mayanksolan@gmail.com>
2019-12-06 07:33:46 +0530

    user model and password hash

--- Projects/moviebuff/routes/api/users.js
+++ Projects/moviebuff/routes/api/users.js
@@ -1,9 +1,52 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+
+const User = require("../../models/User");
 
 // @route   GET api/users..
 // @desc    Test route
 // @access  Public
 router.get("/", (req, res) => res.send("User route"));
+
+// @route   POST api/users..
+// @desc    Register user
+// @access  Public
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+      user = new User({ name, email, password });
+      //encrypt pwd
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      console.log(user);
+      //return jwt
+    } catch (error) {}
+  }
+);
 
 module.exports = router;
