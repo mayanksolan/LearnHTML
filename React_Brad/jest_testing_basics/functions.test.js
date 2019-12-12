3471a509af0cf87bd53b8ee53c7e74c13b7d5292
Mayank Solan <mayanksolan@gmail.com>
2019-12-12 16:53:54 +0530

    jest

--- React_Brad/jest_testing_basics/functions.test.js
+++ React_Brad/jest_testing_basics/functions.test.js
@@ -42,12 +42,12 @@ test("Admin should be in usernames", () => {
 });
 
 // Promise
-// test("User fetched name should be Leanne Graham", () => {
-//   expect.assertions(1);
-//   return functions.fetchUser().then(data => {
-//     expect(data.name).toEqual("Leanne Graham");
-//   });
-// });
+test("User fetched name should be Leanne Graham", () => {
+  expect.assertions(1);
+  return functions.fetchUser().then(data => {
+    expect(data.name).toEqual("Leanne Graham");
+  });
+});
 
 // Async Await
 test("User fetched name should be Leanne Graham", async () => {
