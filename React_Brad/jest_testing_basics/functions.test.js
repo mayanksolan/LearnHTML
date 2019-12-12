8e76e17e8c43ba4a512aa3e49b3536054c368ee9
Mayank Solan <mayanksolan@gmail.com>
2019-12-12 13:36:56 +0530

    basics

--- React_Brad/jest_testing_basics/functions.test.js
+++ React_Brad/jest_testing_basics/functions.test.js
@@ -3,3 +3,40 @@ const functions = require("./functions");
 test("Adds 2 + 2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+test("Adds 2 + 2 to not equal 5", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
+});
+
+test("Should be Null", () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+test("Should be falsy", () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+});
+
+test("user should be Mayank Solan object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Mayank",
+    lastName: "Solan"
+  });
+});
+
+//less then greater then
+test("Should be under 1600", () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+});
+
+//Regex
+test("There is no I in team", () => {
+  expect("team").not.toMatch(/I/);
+});
+
+//Arrays
+test("Admin should be in usernames", () => {
+  usernames = ["john", "mayank", "admin"];
+  expect(usernames).toContain("admin");
+});
